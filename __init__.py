0f86e5208d15002d1962d8a987fbdacc6cf1209d
liliane-ibr <149819098+liliane-ibr@users.noreply.github.com>
2025-10-27 15:04:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,13 @@
-from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+from cryptography.fernet import Fernet, InvalidToken
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
+# Génération d'une clé unique à chaque démarrage
 key = Fernet.generate_key()
 f = Fernet(key)
 
@@ -19,6 +16,12 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
-if __name__ == "__main__":
-  app.run(debug=True)#test
+
+
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+    try:
+        valeur_bytes = f.decrypt(token.encode())  # Déchiffre le token
+        valeur = valeur_bytes.decode()            # Conversion bytes -> str
+        ret
+
